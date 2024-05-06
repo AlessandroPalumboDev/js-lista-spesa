d983af8ca648033df7e480c99b752e29c3a7fff0
AlessandroPalumboDev <alessandropalumbo.dev@gmail.com>
2024-05-06 15:46:23 +0200

    [UPD] Dichiarazione variabili

---
+++ js/main.js
@@ -0,0 +1,11 @@
+'use stricti';
+
+// Creo array della lista della spesa
+const lista = ['pasta', 'cioccolata', 'porchetta', 'tovaglioli', 'trippa', 'pomodori', 'salsa verde'];
+
+// Creo l'elemenrto html (ul) e lo richiamo creando una variabile
+const ul = document.getElementById('lista-spesa');
+
+// Creo una variabile esterna al ciclo per tenere il conto
+let count = 0;
+
